72516b868ec03dddfa85500da9e1bd7bf39abed1
Stephen Guild <s.guild2@students.clark.edu>
2020-06-10 12:38:13 -0700

    Mod 8 Lab 2

---
+++ ClassDemo0610.py
@@ -0,0 +1,27 @@
+"""
+CTEC 121
+<Stephen Guild>
+<Mod 8 Lab 2>
+<assignment/lab description
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    '''
+    #myList=[Car("make","model",2020), car()]
+    myList=[]
+    for i in range(5):
+        #get make
+        #get model
+        #get year
+        myList.append(car(make, model, year))
+    '''
+
+    
+
+main()    
---
+++ roller.py
@@ -0,0 +1,18 @@
+#roller.py
+
+from die import Die
+from button import Button
+
+def main():
+    #d=Die()
+    #b=Button()
+
+    #create application window
+
+    #create objects
+
+    #event loop
+        #wait for mouse click on buttons
+    
+    #close app window
+main()
